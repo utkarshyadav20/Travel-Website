89aee53c9d68b65ae4f4e16fdef3d9555cda2f8c
RunawayGOD <20022002utkarsh@gmail.com>
2025-06-06 16:08:13 +0530

    added package.json

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://travel-website-zru2.onrender.com/api';
 
 export const fetchDestinations = async () => {
   const response = await axios.get(`${API_URL}/destinations`);
